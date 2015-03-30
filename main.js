ec6f34dfebda9d5c504873f27000cbddf95f06d2
Zhang Yiwen <yiwen@pigeonlab.com>
2015-03-30 18:15:41 +0800

    add cors

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 app = require('express.io')()
 app.http().io()
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+
+app.use(allowCrossDomain);
+
+
 // Setup the ready route, and emit talk event.
 app.io.route('ready', function(req) {
     req.io.emit('talk', { 
